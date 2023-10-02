89c576eb9e351efb1df76e48fc6f18f52b81483c
Ciarán Murray <cjm@niva-dk.dk>
2023-10-02 21:49:42 +0200

    Updated function ss(): ss=function(Distace, params=list(a, b))
    New function ss_parameters():
    Returns parameters for ss() function
    from given stream charateristics

--- main.R
+++ main.R
@@ -19,3 +19,18 @@ p1
 p2 <- tm_shape(r3)+
   tm_raster(legend.show = F, palette = "-OrRd" ) 
 p2
+
+tmap::tmap_save(p2, file="figures/example.png", width=12, height=14, units="cm", dpi=300)
+
+
+# we have two new functions:
+#
+# ss(Distance, a, b) - calculates stream signature as function of distance from stream
+#
+# ss_parameters() returns the parameters a and b for the function the 
+#
+# examples:
+
+ss_parameters(method="Dispersal", print_coefficients=T)
+
+ss_parameters(method="Dispersal", organism="Caddisflies", model=2, print_coefficients=T)
